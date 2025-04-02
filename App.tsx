ed5c240cb4f55509e777bc86808e6264acac9c0c
Andrés Eduardo Sánchez Estévez <andrespersonaluser@gmail.com>
2025-04-02 14:43:44 -0500

    feat: Evitar algunos cambios de tema por el sistema

--- App.tsx
+++ App.tsx
@@ -6,13 +6,25 @@ import KickoffStackNavigation from './app/navigation/kickoff-stack.navigation'
 import { I18nextProvider } from 'react-i18next'
 import i18n from './i18n/i18next'
 import Toast from 'react-native-toast-message'
-import { PaperProvider } from 'react-native-paper'
+import { DefaultTheme, PaperProvider } from 'react-native-paper'
 
 export default function App() {
+
+  const lightTheme = {
+    ...DefaultTheme,
+    dark: false, 
+    colors: {
+      ...DefaultTheme.colors,
+      background: 'white', 
+      surface: 'white', 
+      text: 'black', 
+    },
+  };
+
   return (
     <I18nextProvider i18n={i18n}>
       <SafeAreaView style={{ flex: 1 }}>
-        <PaperProvider>
+        <PaperProvider theme={lightTheme}>
           <NavigationContainer>
             <KickoffStackNavigation />
           </NavigationContainer>
